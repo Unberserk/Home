e17e059b443e22884066ed07fc3a16351f58377d
Unberserk <104165736+Unberserk@users.noreply.github.com>
2025-07-20 16:39:42 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
 const CACHE_NAME = 'berserk-cache-v1';
 const ASSETS = [
+  '/',
+  '/index.html',
   '/f.html',
+  '/g.html',
+  '/u.html',
+  '/d.html',
+  '/e.html',
+  '/settings.html',
   '/Assests/CSS/main.css',
   '/Assests/CSS/f.css',
   '/Assests/JS/f.js',
-  '/manifest.json',
   '/Assests/Imgs/Logo.png',
   'https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
+    caches.keys().then(keys =>
       Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
@@ -27,10 +33,9 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return (
-        cachedResponse ||
-        fetch(event.request).catch(() => new Response('You are offline', { status: 503, statusText: 'Offline' }))
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).catch(() =>
+        new Response('You are offline', { status: 503, statusText: 'Offline' })
       );
     })
   );
